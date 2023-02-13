2e55933625b11598394d2e07227f36a1bb6c7877
Raphael <raphaelaugusto@tutanota.com>
2023-02-13 13:40:38 -0300

    fix: move func setup

---
+++ PoliServices/Model/SetupModel.swift
@@ -0,0 +1,16 @@
+//
+//  SetupModel.swift
+//  PoliServices
+//
+//  Created by Raphael Augusto on 13/02/23.
+//
+
+import Foundation
+
+struct SetupData {
+    let toCompleteService: String
+    let hasService: Bool
+    let serviceName: String
+    let serviceColor: String
+    let serviceDate: String
+}
